b6b7953b0a8ed203b12b58c1b036bb2d00d8c06a
Rodrigo Souza <rodrigo.augusto93@gmail.com>
2020-07-23 19:11:26 -0300

    Create appointment

--- appgobarber/src/hooks/auth.tsx
+++ appgobarber/src/hooks/auth.tsx
@@ -13,9 +13,7 @@ interface User {
   id: string;
   name: string;
   email: string;
-  avatar: string;
-  created_at: Date;
-  updated_at: Date;
+  avatar_url: string;
 }
 
 interface AuthState {
@@ -49,6 +47,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -70,6 +70,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
