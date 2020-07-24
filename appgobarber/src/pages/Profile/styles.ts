2e52942a8d77b9aa736f435d118810125952f4ac
Rodrigo Souza <rodrigo.augusto93@gmail.com>
2020-07-23 21:33:32 -0300

    Update profile

---
+++ appgobarber/src/pages/Profile/styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+
+import { Form as UnfomForm } from '@unform/mobile';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  margin-top: 64px;
+`;
+
+export const UserAvatarButton = styled.TouchableOpacity`
+  margin-top: 64px;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 186px;
+  height: 186px;
+  border-radius: 88px;
+  align-self: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 24px 0;
+`;
+
+export const Form = styled(UnfomForm)`
+  width: 100%;
+`;
